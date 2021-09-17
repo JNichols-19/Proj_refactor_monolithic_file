8f8ebf98da3273b606e3b296bf059b34cc8dc8f1
JNichols-19 <julia.nichols.19@cnu.edu>
2021-09-17 18:49:07 -0400

    Moved all functions and added guards

--- includes/fileio.h
+++ includes/fileio.h
@@ -1,7 +1,11 @@
-#include <constants.h>
+#include "../includes/constants.h"
 #include <string>
 #include <vector>
 
 //attempt to open file 'filename' and read in all data
 //returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
 int load(const std::string filename, std::vector<process> &myProcesses);
+
+//attempt to create or open file 'filename' to write all data to
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int save(const std::string filename, std::vector<process> &myProcesses);
